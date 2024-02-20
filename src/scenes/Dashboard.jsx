11aea8ead36c9510e8fe31d5efd385998d8e8f85
jannet18 <janetngei18@gmail.com>
2024-02-20 13:31:08 +0300

    header component

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "../components/Header";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" />
+      </Box>
+    </Box>
+  );
 }
 
 export default Dashboard;
