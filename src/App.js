56c0fc0a6f8d76f3dfccca9d91943c19ca0589e1
jannet18 <janetngei18@gmail.com>
2024-02-20 13:19:17 +0300

    sidebar created with react-pro-sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Topbar from "./global/Topbar";
 import { ColorModeContext } from "./theme";
 import { useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/Dashboard";
+import Sidebar from "./global/Sidebar";
+import { useState } from "react";
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
